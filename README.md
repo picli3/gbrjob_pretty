1ab7e39858bf386d406574838cc3e66fd8a6731f
Maykol Rey <maykolrey@gmail.com>
2022-03-20 19:54:27 -0500

    version sin sin pandas

---
+++ README.md
@@ -0,0 +1,1 @@
+# pretty Gerber X2
---
+++ gbrjobToPdf.py
@@ -0,0 +1,89 @@
+import json
+import csv  
+
+header = ['Layers', 'PadToPad', 'PadToTrack', 'TrackToTrack','MinLineWidth']
+MaterialStackup = ['Type','Name','Thickness (mm)','Material','Color','DielectricConstant']
+header2=['DesignRules']
+header3=['MaterialStackup']
+header4=['GeneralSpecs']
+header5=['Dimentions']
+header6=['LayerNumber','BoardThickness','Finish','ImpedanceControlled']
+
+fj = open('ethernet_audio-job.gbrjob')
+# returns JSON object as
+# a dictionary
+data = json.load(fj)
+ 
+# Iterating through the json
+# list
+
+name = [data["GeneralSpecs"]["ProjectId"]["Name"]]
+GUID = [data["GeneralSpecs"]["ProjectId"]["GUID"]]
+Revision = [data["GeneralSpecs"]["ProjectId"]["Revision"]]
+Dimention = [data["GeneralSpecs"]["Size"]["X"],data["GeneralSpecs"]["Size"]["Y"]]
+ci = False
+try:
+	if data["GeneralSpecs"]["ImpedanceControlled"]:
+		ci=True
+except Exception as e:
+	print(e)
+
+
+GeneralSpecs= [data["GeneralSpecs"]["LayerNumber"],data["GeneralSpecs"]["BoardThickness"],data["GeneralSpecs"]["Finish"],ci]
+
+
+with open('config_electrical.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(name)
+    writer.writerow(GUID)
+    writer.writerow(Revision)
+    writer.writerow('')
+    writer.writerow(header6)
+    writer.writerow(GeneralSpecs)
+    writer.writerow('')
+    writer.writerow(header5)
+    writer.writerow(Dimention)
+    writer.writerow('')
+    writer.writerow(header2)
+    writer.writerow(header)
+    dataForm = []
+    for i in data['DesignRules']:
+        dataForm.append(i['Layers'])     
+        dataForm.append(i['PadToPad'])
+        dataForm.append(i['PadToTrack'])
+        dataForm.append(i['TrackToTrack'])
+        if 'MinLineWidth' in i:
+        	dataForm.append(i['MinLineWidth'])
+        else:
+        	dataForm.append('-')
+        writer.writerow(dataForm)
+        dataForm.clear()
+    writer.writerow('')
+    writer.writerow(header3)
+    writer.writerow(MaterialStackup)
+    for i in data['MaterialStackup']:
+    	dataForm.append(i['Type'])
+    	dataForm.append(i['Name'])
+    	if 'Thickness' in i:
+    		dataForm.append(i['Thickness'])
+    	else:
+    		dataForm.append('-')
+    	if 'Material' in i:
+    		dataForm.append(i['Material'])
+    	else:
+    		dataForm.append('-')
+    	if 'Color' in i:
+    		dataForm.append(i['Color'])
+    	else:
+    		dataForm.append('-')
+    	if 'DielectricConstant' in i:
+    		dataForm.append(i['DielectricConstant'])
+    	else:
+    		dataForm.append('-')
+
+    	writer.writerow(dataForm)
+    	dataForm.clear()
+
+# Closing file
+fj.close()
